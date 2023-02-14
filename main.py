b247864d7ca275200eaa4ab6b2495c699313c031
ioan-hedea <hedea.mihnea.2003@gmail.com>
2023-02-14 12:44:45 +0100

    add files

---
+++ main.py
@@ -0,0 +1,22 @@
+import tweepy
+
+from other import keys
+
+
+def api():
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
+    return tweepy.API(auth)
+
+
+def tweet(api: tweepy.API, message: str, image_path=None):
+    if image_path:
+        api.update_status_with_media(message, image_path)
+    else:
+        api.update_status(message)
+    print('successful tweet')
+
+
+if __name__ == '__main__':
+    api = api()
+    tweet(api, 'This is a test tweet')
