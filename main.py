9a623c5da33a8607ec7f2ff3190738da1af97122
ioan-hedea <i.m.hedea@student.tudelft.nl>
2023-06-06 15:42:57 +0200

    add working ioan_bot_bot

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from asyncore import dispatcher
-
-from telegram.ext import Updater, CommandHandler
-
-from bot import start_command
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from typing import Final
+"""
 
 
 def main():
@@ -14,3 +13,73 @@ def main():
 
     updater.start_polling()
     updater.idle()
+
+
+
+
+"""
+token: Final = "6061354792:AAF7UHyG5gboTAKVU8aX8Vw9W9xUu1Bikaw"
+
+bot_username: Final = "@ioan_bot_bot"
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! What book do you want me to tell you about?")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I am a nice geeky bot. TEll me something to start the conversation")
+
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Charles Dickens was a brit from the 19th century")
+
+
+
+def handle_response(text: str) -> str:
+    processed:str = text.lower()
+    if "hello" in processed:
+        return "Hi"
+    if "morometii" in processed:
+        return "Este scris de Marin Preda"
+    if "maora cu noroc" in processed:
+        return "ioan slavici"
+    return "I am sorry, can you repeat?"
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message: str = update.message.chat.type
+    text:str = update.message.text
+
+    print(f'User ({update.message.chat.id}) in {message}: "{text}"')
+
+    if message == "group":
+        if bot_username in text:
+            new_text: str = text.replace(bot_username, "")
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response:str = handle_response(text)
+
+    print("response sent")
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"update{update} caused error {context.error}")
+
+
+if __name__ == '__main__':
+    app = Application.builder().token(token).build()
+
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('start', custom_command))
+    app.add_handler(CommandHandler('start', help_command))
+
+
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    app.add_error_handler(error)
+
+
+    print("polling")
+    app.run_polling(poll_interval=3)
